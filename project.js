70b4f910e264dd55e7365c15e95d064eca4c68f7
Venkatesh <vullagantivenkatesh@gmail.com>
2022-08-09 18:25:15 +0530

    added all the files

---
+++ project.js
@@ -0,0 +1,11 @@
+// const slides = document.querySelectorAll(".slide");
+
+// const slider = document.querySelector(".images-slider");
+// slider.style.transform = "scale(0.5)";
+// slider.style.overflow = "visible";
+
+// slides.forEach(
+//   (slide, index) => (slide.style.transform = `translate(${100 * index})`)
+// );
+
+// 0% 100% 200% ..
